9d2a5ca7a86875038e38143ffbba6e690e2d0a40
Angel Nunez <agonzalez@bit2main.com>
2019-04-29 21:26:50 -0500

    Segundo intento

--- bot.js
+++ bot.js
@@ -1,12 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // API Token Telegram
-const token = '781753975:AAE-ZfNuvKDDbZf0uTlAeDAGNto0EU9QZrM';
+const token = '862893233:AAEcnBmybxFy39fPxlTC3c2OFRGp5eHcVFc';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
 // Cuando mandes el mensaje "Hola" reconoce tú nombre y genera un input: Hola Daniel
-bot.onText(/^\/hola/, (msg) => {
-     bot.sendMessage(msg.chat.id, "Hola  " + msg.from.first_name);
- });
+bot.on('message', (msg) => {
+var Hi = "hi";
+if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id, "Hello dear user");
+}
+var bye = "bye";
+if (msg.text.toString().toLowerCase().includes(bye)) {
+    bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+}
+var robot = "I'm robot";
+if (msg.text.indexOf(robot) === 0) {
+    bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
+}
+});
