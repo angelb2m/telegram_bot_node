668a373b8e322ddcd879339f741ca01dbc9d87ba
Angel Nunez <agonzalez@bit2main.com>
2019-05-01 09:28:09 -0500

    debug

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-"use strict"
-
 var restify = require('restify');
-var server = restify.createServer();
 
-server.get('/', (req,res,next) =>
-{
-    res.json([{sku: 'ab48cicj36734', asin: 'B015E8UTIU', upc: '888462500449',
-        title: 'Apple iPhone 6s 64 GB US Warranty Unlocked Cellphone - Retail Packaging (Rose Gold)',
-        image: 'http://ecx.images-amazon.com/images/I/91DpCeCgSBL._SL1500_.jpg' }]);
-    next();
-});
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
 
 server.listen(8080, function() {
-    console.log('%s listening at %s', server.name, server.url);
+  console.log('%s listening at %s', server.name, server.url);
 });
-
-module.exports = server;
